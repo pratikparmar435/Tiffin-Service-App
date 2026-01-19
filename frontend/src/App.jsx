be7fd3c95fc1519647db7c43f2d6ea24bfa09362
pratikparmar435 <prateekparmar435@gmail.com>
2026-01-19 12:27:46 +0530

    Home page footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BackgroundImg } from "./home-page/hero-section/BackgroundImg";
 import SubHero from "./home-page/sub-hero-section/SubHero";
 import Navbar from "./home-page/navigation-section/Navbar";
+import HomeFooter from "./home-page/footer/HomeFooter";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <BackgroundImg />
       <SubHero />
+      <HomeFooter />
     </>
   );
 }
