88458409da7f744d1b6aa5733b91265af268af6c
pratikparmar435 <prateekparmar435@gmail.com>
2026-02-18 21:32:31 +0530

    Search the tiffin services in city feature implemented

--- frontend/src/search-page/MainContent.jsx
+++ frontend/src/search-page/MainContent.jsx
@@ -1,200 +1,95 @@
-import CardMapping from './CardMapping'
-
-import FooterSection from './FooterSection'
-import NavbarOfViewMenuCart from './NavbarOfViewMenuCart'
-import Sidebar from './Sidebar'
-import React, { useState } from 'react'
+import CardMapping from "./CardMapping";
+import { useLocation } from "react-router-dom";
+import FooterSection from "./FooterSection";
+import NavbarOfViewMenuCart from "./NavbarOfViewMenuCart";
+import Sidebar from "./Sidebar";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function MainContent() {
-    const [page, setPage] = useState(1)
-    const Arr = [{
-        img: "https://img.freepik.com/free-photo/top-view-fast-food-mix-mozzarella-sticks-club-sandwich-hamburger-mushroom-pizza-caesar-shrimp-salad-french-fries-ketchup-mayo-cheese-sauces-table_141793-3998.jpg?semt=ais_hybrid&w=740&q=80",
-        title: "Ghar ka swad",
-        rating: 4.5,
-        icon: "star",
-        time: "⏳Launch: 12PM-2PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹80/meal",
-        number: 2000,
-        button: "View Menu  ›"
+  const [page, setPage] = useState(1);
+  const [meals, setMeals] = useState([]);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const city = queryParams.get("city");
 
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBvlxtQfYRJv7rLVaiSpiK6SRINhWUGxFCew&s",
-        title: "Mom's Kitchen",
-        rating: 4.2,
-        icon: "star",
-        time: "⏳Launch: 1PM-3AM",
-        t2: "STARTING FROM",
-        t3: "WEEKLY",
-        rupee: "₹800/meal",
-        number: 3000,
-        button: "View Menu  ›"
+  useEffect(() => {
+    if (!city) return;
 
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZf1GDhM9--1KDQ1F7FBasFbqgnTfsBAGozA&s",
-        title: "Healthy Bites",
-        rating: 2.5,
-        icon: "star",
-        time: "⏳Launch: 10PM-2PM",
-        t2: "STARTING FROM",
-        t3: "Yearly",
-        rupee: "₹70/meal",
-        number: 4000,
-        button: "View Menu  ›"
+    axios
+      .get(`http://localhost:8080/search?city=${city}`)
+      .then((res) => {
+        setMeals(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [city]);
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NavbarOfViewMenuCart city={city} />
 
-    }, {
-        img: "https://cdn.britannica.com/98/235798-050-3C3BA15D/Hamburger-and-french-fries-paper-box.jpg",
-        title: "Spice Route",
-        rating: 4.5,
-        icon: "star",
-        time: "⏳Launch: 12PM-2PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹80/meal",
-        number: 2000,
-        button: "View Menu ›"
+      <div className="max-w-8xl mx-auto flex flex-col md:flex-row">
+        <Sidebar />
 
-    }, {
-        img: "https://www.foodandwine.com/thmb/fjNakOY7IcuvZac1hR3JcSo7vzI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/FAW-recipes-pasta-sausage-basil-and-mustard-hero-06-cfd1c0a2989e474ea7e574a38182bbee.jpg",
-        title: "South Tadka",
-        rating: 5.00,
-        icon: "star",
-        time: "⏳Launch: 12PM-2:30PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹60/meal",
-        number: 1000,
-        button: "View Menu  ›"
-
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQynKAtD1jF4MElJGeQiXp9IxgDkrY8Z7tKA&s",
-        title: "Daily Fresh",
-        rating: 4.5,
-        icon: "star",
-        time: "⏳Launch: 12:30PM-2PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹95/meal",
-        number: 5000,
-        button: "View Menu ›"
-
-    },
-    ]
-    const Arr2 = [{
-        img: "https://miro.medium.com/v2/resize:fit:1400/0*0zGCVuvlLlbfEeJM.",
-        title: "Taste of Real",
-        rating: 4.0,
-        icon: "star",
-        time: "⏳Launch: 2PM-12PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹70/meal",
-        number: 12000,
-        button: "View Menu  ›"
-
-    }, {
-        img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/49/bc/84/api-api-cafe.jpg?w=1200&h=1200&s=1",
-        title: "Trisha's Kitchen",
-        rating: 3.2,
-        icon: "star",
-        time: "⏳Launch: 1PM-7AM",
-        t2: "STARTING FROM",
-        t3: "WEEKLY",
-        rupee: "₹8000/meal",
-        number: 3000,
-        button: "View Menu  ›"
-
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ0dHIzSTcLXOANSqFqsW1dS-wUdi2i2J2Ng&s",
-        title: "Healthy Bites",
-        rating: 2.8,
-        icon: "star",
-        time: "⏳Launch: 10PM-2AM",
-        t2: "STARTING FROM",
-        t3: "Yearly",
-        rupee: "₹70/meal",
-        number: 4000,
-        button: "View Menu  ›"
-
-    }, {
-        img: "https://cdn.prod.website-files.com/64e392bed89d850c3f9dec8e/68821bc8e2057834072dc402_009%20balanced_meal_plate.jpeg",
-        title: "Adani's Dhaba",
-        rating: 1.5,
-        icon: "star",
-        time: "⏳Launch: 3PM-1AM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹90/meal",
-        number: 11000,
-        button: "View Menu ›"
-
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv3071_aBnZLnCsQp4SJ_G1j5w0XOs7gfZog&s",
-        title: "Punjabi Tadka",
-        rating: 5.0,
-        icon: "star",
-        time: "⏳Launch: 12PM-2:15PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹650/meal",
-        number: 100,
-        button: "View Menu  ›"
-
-    }, {
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoPt6KdSEVdeQLLkceuKhGGKuLZaSY2KTOUA&s",
-        title: "Ruby's Restorent",
-        rating: 1.5,
-        icon: "star",
-        time: "⏳Launch: 12:30PM-2PM",
-        t2: "STARTING FROM",
-        t3: "MONTHLY",
-        rupee: "₹95/meal",
-        number: 5000,
-        button: "View Menu ›"
-
-    },
-    ]
-    return (
-        <div>
-            <NavbarOfViewMenuCart></NavbarOfViewMenuCart>
-
-            <div className='flex'>
-                <Sidebar></Sidebar>
-                <div className='w-full'>
-                    <div className='flex justify-between w-full mt-4 mr-4'>
-                        <div className='gap-5 ml-13'>
-                            <strong className='text-2xl'>Tiffin Services In Ahemdabad</strong><br />
-                            <small className='text-[#f47519]'>
-                                124 providers available for your location
-                            </small>
-                        </div>
-                        <div className='flex gap-4'>
-                            <small className='mt-2 text-[#ae8667]'>Sort by:</small>
-                            <select name="sort" id="sort"
-                                className='h-8 w-45 rounded-2xl py-1 px-3 bg-[#f4ece7]'>
-                                <option value="popularity">Popularity</option>
-                                <option value="popularity">Affordable</option>
-                            </select>
-                        </div>
-                    </div>
-                    {page === 1 && <CardMapping data={Arr} />}
-                    {page === 2 && <CardMapping data={Arr2} />}
-
-                </div>
-
+        <main className="flex-1 p-4 md:p-8">
+          <header className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8 gap-4">
+            <div>
+              <h1 className="text-2xl md:text-3xl font-bold text-gray-900">
+                Tiffin Services in {city}
+              </h1>
+              <p className="text-[#f47519] font-medium">
+                {meals.length} providers found
+              </p>
             </div>
-            <div className='flex justify-center gap-4 mt-5 ml-35 '>
-                <button onClick={() => setPage(1)} className='bg-[#f8f8f5] h-10 cursor-pointer border  border-gray-300 w-10 rounded-xl hover:bg-[#f47519]'>1</button>
-                <button
-                    onClick={() => setPage(2)} className='bg-[#f8f8f5] h-10 cursor-pointer border border-gray-300 w-10 rounded-xl hover:bg-[#f47519]'>2</button>
-            </div>
-            <div className='mt-5'>
-                <FooterSection></FooterSection>
-            </div>
-        </div>
 
-    )
+            <div className="flex items-center gap-2 bg-white p-2 rounded-lg shadow-sm border border-gray-200">
+              <span className="text-sm text-gray-500">Sort:</span>
+              <select className="bg-transparent outline-none font-semibold text-gray-700">
+                <option>Popularity</option>
+                <option>Price: Low to High</option>
+              </select>
+            </div>
+          </header>
+
+          {/* Responsive Grid */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+            {meals.map((meal, idx) => (
+              <CardMapping meal={meal} key={idx} />
+            ))}
+          </div>
+
+          {/* Pagination */}
+          <footer className="mt-12 flex justify-center gap-2">
+            {[1, 2, 3].map((num) => (
+              <button
+                key={num}
+                className="w-10 h-10 rounded-lg border border-gray-300 hover:bg-[#f47519] hover:text-white transition-all"
+              >
+                {num}
+              </button>
+            ))}
+          </footer>
+        </main>
+      </div>
+      <FooterSection />
+    </div>
+  );
 }
 
-export default MainContent
+export default MainContent;
--- frontend/src/search-page/NavbarOfViewMenuCart.jsx
+++ frontend/src/search-page/NavbarOfViewMenuCart.jsx
@@ -1,38 +1,50 @@
-import React from 'react'
 import { Search } from "lucide-react";
-import { MapPin } from 'lucide-react';
-import { Heart } from 'lucide-react';
-import { User } from 'lucide-react';
-function NavbarOfViewMenuCart() {
-    return (
-        <div className='bg-white w-full h-15 justify-between flex' >
-            <div className='flex h-full items-center gap-2 w-[20%]'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpfQ9OGMdZzHWxV-Y4fkFA5ZK22YJo1lbfSw&s" className='h-8 w-8 ml-5 rounded-4xl' />
-                <strong>TiffinHub</strong>
-            </div>
-            <div className='w-[60%] flex items-center '>
-                <div className='flex rounded-3xl  bg-[#f4ece7] w-full py-1.5 '>
-                    <div className='flex gap-2 items-center'>
-                        <MapPin size={24} color="#f47519" strokeWidth={3} className='ml-5' />
-                        <strong>Gota,Ahemdabad</strong>
-                    </div>
-                    <div className='flex gap-4 '>
-                        <Search color="#ae8667" className='ml-10' />
-                        <input type="text" placeholder='Search tiffin services...' className='text-[#88512a] border-none' />
-                    </div>
-                </div>
-            </div>
-            <div className=' w-[20%] flex items-center ml-15'>
-                <div className='flex gap-5 font-bold '>
-                    <a href="#">My orders</a>
-                    <a href="#">Help</a>
-                    <div className='rounded-4xl bg-[#f4ece7] h-7 w-7 flex justify-center items-center'><Heart size={20} strokeWidth={3} /></div>
-                    <div className='rounded-4xl bg-[#f4ece7] h-7 w-7 flex justify-center items-center'><User size={20} strokeWidth={3} /></div>
-                </div>
-            </div>
-
+import { MapPin } from "lucide-react";
+import { Heart } from "lucide-react";
+import { User } from "lucide-react";
+function NavbarOfViewMenuCart({ city }) {
+  return (
+    <div className="bg-white w-full py-2.5 px-5 items-center sticky top-0 shadow-sm justify-between flex">
+      <div className="flex h-full items-center gap-2 w-[20%]">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpfQ9OGMdZzHWxV-Y4fkFA5ZK22YJo1lbfSw&s"
+          className="h-12 w-12 rounded-4xl"
+        />
+        <strong className="text-2xl">TiffinHub</strong>
+      </div>
+      <div className="w-[60%] flex items-center ">
+        <div className="flex rounded-3xl py-3 bg-[#f4ece7] w-full">
+          <div className="flex gap-2 items-center">
+            <MapPin
+              size={24}
+              color="#f47519"
+              strokeWidth={3}
+              className="ml-5"
+            />
+            <span className="font-semibold text-xl">{city}</span>
+          </div>
+          <div className="flex gap-4 ">
+            <Search color="#ae8667" className="ml-10" />
+            <input
+              type="text"
+              placeholder="Search tiffin services..."
+              className="text-[#88512a] outline-none"
+            />
+          </div>
         </div>
-    )
+      </div>
+      <div className="w-[20%] flex items-center ml-15 gap-5 font-bold">
+        <a href="#">My orders</a>
+        <a href="#">Help</a>
+        <div className="rounded-4xl bg-[#f4ece7] p-2 flex justify-center items-center">
+          <Heart size={20} strokeWidth={3} />
+        </div>
+        <div className="rounded-4xl bg-[#f4ece7] p-2 flex justify-center items-center">
+          <User size={20} strokeWidth={3} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NavbarOfViewMenuCart
+export default NavbarOfViewMenuCart;
